fb66ccc4d13ea248d9d7d218fec155299193f46f
Carlos Alfonso Chong Zapata <112518890+carloschong10@users.noreply.github.com>
2024-08-05 19:24:42 -0500

    Implementando la clase de servicio (Service)

---
+++ src/main/java/org/chong/test/springboot/app/services/CuentaServiceImpl.java
@@ -0,0 +1,54 @@
+package org.chong.test.springboot.app.services;
+
+import org.chong.test.springboot.app.models.Banco;
+import org.chong.test.springboot.app.models.Cuenta;
+import org.chong.test.springboot.app.repositories.BancoRepository;
+import org.chong.test.springboot.app.repositories.CuentaRepository;
+
+import java.math.BigDecimal;
+
+public class CuentaServiceImpl implements CuentaService {
+
+    private CuentaRepository cuentaRepository;
+    private BancoRepository bancoRepository;
+
+    public CuentaServiceImpl(CuentaRepository cuentaRepository, BancoRepository bancoRepository) {
+        this.cuentaRepository = cuentaRepository;
+        this.bancoRepository = bancoRepository;
+    }
+
+    @Override
+    public Cuenta findById(Long id) {
+        return cuentaRepository.findById(id);
+    }
+
+    @Override
+    public int revisarTotalTransferencias(Long bancoId) {
+        Banco banco = bancoRepository.findById(bancoId);
+
+        return banco.getTotalTransferencias();
+    }
+
+    @Override
+    public BigDecimal revisarSaldo(Long cuentaId) {
+        Cuenta cuenta = cuentaRepository.findById(cuentaId);
+
+        return cuenta.getSaldo();
+    }
+
+    @Override
+    public void transferir(Long cuentaOrigen, Long cuentaDestino, BigDecimal monto, Long bancoId) {
+        Banco banco = bancoRepository.findById(bancoId);
+        int totalTransferencias = banco.getTotalTransferencias();
+        banco.setTotalTransferencias(++totalTransferencias);
+        bancoRepository.update(banco);
+
+        Cuenta cuentaOri = cuentaRepository.findById(cuentaOrigen);
+        cuentaOri.debito(monto);
+        cuentaRepository.update(cuentaOri);
+
+        Cuenta cuentaDest = cuentaRepository.findById(cuentaDestino);
+        cuentaDest.credito(monto);
+        cuentaRepository.update(cuentaDest);
+    }
+}
